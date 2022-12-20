b3de95614b8e7ce6808c1cb17929cca3ac3b0bea
EliasOlg <eliasjolguin@gmail.com>
2022-12-20 12:29:56 -0300

    excursiones y error 404

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-
-
+import { ExcursionesModule } from '../excursiones/excursiones.module';
+import { ExcursionesComponent } from '../excursiones/excursiones.component';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomeComponent],
+  imports: [CommonModule, ExcursionesModule],
 })
-export class HomeModule { }
+export class HomeModule {}
--- src/app/pages/page-not-found/page-not-found.component.ts
+++ src/app/pages/page-not-found/page-not-found.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.css']
+  styleUrls: ['./page-not-found.component.css'],
 })
 export class PageNotFoundComponent implements OnInit {
+  pageX: number = 0;
+  pageY: number = 0;
+  mouseY = 0;
+  mouseX = 0;
+  yAxis = 0;
+  xAxis = 0;
+  clase: string;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  @HostListener('document:mousemove', ['$event'])
+  getMove(e: any) {
+    console.log(e);
+    this.mouseY = e.pageY;
+    this.yAxis = ((this.pageY / 2 - this.mouseY) / this.pageY) * 300;
+    this.mouseX = e.pageX / -this.pageX;
+    this.xAxis = -this.mouseX * 100 - 100;
+    this.clase = `transform: translate(${this.xAxis}%,-${this.yAxis}%)`;
+    console.log(this.clase);
   }
-
 }
